0d3f35dc8f28fdfd97033ab1ad454f1c32b5acab
Justin <iceyang1007@gmail.com>
2019-06-27 14:22:00 +0800

    create directory for code. delete "web"

--- web/cmd/server.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/iceyang/m-go-cookbook/web/internal/engine"
-
-func main() {
-	engine.Default().Run(":7900")
-}
--- web/internal/model/example.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Example struct {
-	Name string
-	Age  int
-}
