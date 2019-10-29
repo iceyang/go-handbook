715e20cd1b1e54bcc4585ce73d85475ed336c18a
Justin <iceyang1007@gmail.com>
2019-10-29 23:09:11 +0800

    add content for interface

--- code/08-struct/struct.go
+++ code/08-struct/struct.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Person struct {
 	name string
 	age  int
@@ -28,26 +26,4 @@ type AnonymousField struct {
 	a int
 	b int
 	int
-}
-
-type Cat struct {
-}
-
-func (cat *Cat) Eat() {
-	fmt.Println("Cat is eating")
-}
-
-func (cat *Cat) Sleep() {
-	fmt.Println("Cat is sleeping")
-}
-
-type Dog struct {
-}
-
-func (cat *Dog) Eat() {
-	fmt.Println("Dog is eating")
-}
-
-func (cat *Dog) Sleep() {
-	fmt.Println("Dog is sleeping")
 }
--- code/09-interface/interface.go
+++ code/09-interface/interface.go
@@ -1,6 +1,30 @@
 package main
 
+import "fmt"
+
 type Animal interface {
 	Eat()
 	Sleep()
+}
+
+type Cat struct {
+}
+
+func (cat *Cat) Eat() {
+	fmt.Println("Cat is eating")
+}
+
+func (cat *Cat) Sleep() {
+	fmt.Println("Cat is sleeping")
+}
+
+type Dog struct {
+}
+
+func (cat *Dog) Eat() {
+	fmt.Println("Dog is eating")
+}
+
+func (cat *Dog) Sleep() {
+	fmt.Println("Dog is sleeping")
 }
