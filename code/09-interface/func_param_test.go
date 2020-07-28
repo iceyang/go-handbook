912244d30abcce3d2287e572e5da694450b9f325
Justin <iceyang1007@gmail.com>
2020-07-28 11:00:28 +0800

    add test code

--- code/09-interface/func_param_test.go
+++ code/09-interface/func_param_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func variableLengthParams(params ...interface{}) {
+	fmt.Println(params...)
+
+	// Should Check length first.
+	// fmt.Println(params[0])
+}
+
+func TestVariableLengthParams(t *testing.T) {
+	variableLengthParams()
+	variableLengthParams(nil)
+	variableLengthParams("hello")
+}
