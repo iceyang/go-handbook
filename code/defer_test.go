128b523cb8b9bae5eaa8a5858016d073069be74b
Justin <iceyang1007@gmail.com>
2020-08-14 11:16:51 +0800

    add defer test

--- code/defer_test.go
+++ code/defer_test.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"testing"
+
+	"github.com/fagongzi/log"
 )
 
+func do() {
+	log.Info("Do something finally.")
+}
+
 func TestDefer(t *testing.T) {
-	defer func() {
-		t.Log("Do something finally.")
-	}()
-	t.Log("here we go.")
+	defer do()
+	log.Info("here we go.")
+}
+
+func TestDefer2(t *testing.T) {
+	defer do()
+	if true {
+		return
+	}
+	log.Info("here we go.")
+}
+
+func TestDefer3(t *testing.T) {
+	if true {
+		return
+	}
+	defer do()
+	log.Info("here we go.")
 }
